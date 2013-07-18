43ef5e875677ef5656834bf0d4600de6fffaa139
haden <haden.dude@gmail.com>
2013-07-18 10:24:47 +0100

    changed resolution to 1024x600

--- source/ProjectClass.hx
+++ source/ProjectClass.hx
@@ -17,8 +17,8 @@ class ProjectClass extends FlxGame
 	{
 		var stageWidth:Int = Lib.current.stage.stageWidth;
 		var stageHeight:Int = Lib.current.stage.stageHeight;
-		var ratioX:Float = stageWidth / 640;
-		var ratioY:Float = stageHeight / 480;
+		var ratioX:Float = stageWidth / 1024;
+		var ratioY:Float = stageHeight / 600;
 		var ratio:Float = Math.min(ratioX, ratioY);
 		super(Math.ceil(stageWidth / ratio), Math.ceil(stageHeight / ratio), MenuState, ratio, 30, 30);
 	}
