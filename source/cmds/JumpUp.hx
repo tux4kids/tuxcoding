5f6d7f3577712419a15ec8793c51a9512845c155
haden dude <haden.dude@gmail.com>
2013-09-03 10:40:15 +0100

    (big) Jump Up/Down

---
+++ source/cmds/JumpUp.hx
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2013 Tux4Kids 
+ * Distributed under the terms of the MIT License. 
+ * 
+ * Author(s): 
+ * Abdelhakim Deneche 
+ */
+
+package cmds;
+
+/**
+ * make the character jump up
+ */
+class JumpUp extends Cmd
+{
+	
+	public function new(world:World) 
+	{
+		super(world);
+	}
+	
+	override public function run():Bool
+	{
+		world.player.jumpUp();
+		return false;
+	}
+	
+	override public function canRun():Bool 
+	{
+		var player:Player = world.player;
+		// coordinates of destination of the jump
+		var tileX:Int = player.tileX + (player.facingLeft ? -3 : 3); // facing tileX
+		var tileY:Int = player.tileY - 1;
+		
+		return (world.insideMapAndEmpty(tileX, tileY) && world.insideMapAndFull(tileX, tileY+1));
+	}
+	
+	
+}
